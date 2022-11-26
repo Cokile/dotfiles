3495b4fedb86eb75bc49e15172d8b94ceaeb27fe
Cokile Ceoi <cokileceoi@outlook.com>
2022-11-26 10:47:02 +0800

    chore: adopt m1

--- fish/bootstrap.post.sh
+++ fish/bootstrap.post.sh
@@ -2,14 +2,28 @@
 
 set -e
 
+FISH_PATH=$(command -v fish)
+SHARE_PATH=/opt/share/bin
+
 # set fish as default shell
 if [[ $(cat /etc/shells | grep fish) == "" ]]; then
-    FISH_PATH=$(command -v fish)
     echo "Setting fish as default shell..."
     echo "$FISH_PATH" | sudo tee -a /etc/shells
     chsh -s "$FISH_PATH"
 fi
 echo "Fish is the default shell."
+
+# symlink fish to /opt/share/bin
+# so we can use /opt/share/bin/fish for alacritty on M1 and Intel
+if [[ ! -d $SHARE_PATH ]]; then
+    echo "Creating $SHARE_PATH..."
+    sudo mkdir -p "$SHARE_PATH"
+fi
+if [[ ! -L $SHARE_PATH/fish ]]; then
+    echo "Linking fish to $SHARE_PATH..."
+    sudo ln -s "$FISH_PATH" "$SHARE_PATH/fish"
+fi
+echo "Fish is linked to $SHARE_PATH."
 
 # change theme
 echo "Changing fish theme..."
--- fish/config.fish
+++ fish/config.fish
@@ -11,13 +11,21 @@ set -gx LANG en_US.UTF-8
 set -x EDITOR nvim
 set -x MANPAGER 'nvim +Man!'
 
+# homebrew
+if [ (uname -m) = 'arm64' ]
+    set -x BREW_PATH /opt/homebrew
+else
+    set -x BREW_PATH /usr/local
+end
+set -x PATH $BREW_PATH/bin $PATH
+
 ## go
 set -x GOPATH ~/go
 set -x PATH $GOPATH/bin $PATH
 
 ## ruby
-set -x RUBY_PATH /usr/local/opt/ruby
-set -x GEM_HOME /usr/local/lib/ruby/gems/3.1.0
+set -x RUBY_PATH $BREW_PATH/opt/ruby
+set -x GEM_HOME $BREW_PATH/lib/ruby/gems/3.1.0
 set -x PATH $RUBY_PATH/bin $PATH
 set -x PATH $GEM_HOME/bin $PATH
 
