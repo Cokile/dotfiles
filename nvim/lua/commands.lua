54ee240fa0ec596566fc2d358c35b167ad09c6d5
Cokile Ceoi <cokileceoi@outlook.com>
2023-01-17 20:42:50 +0800

    improve(nvim): change shiftwidth to 2 by default

--- nvim/lua/commands.lua
+++ nvim/lua/commands.lua
@@ -1,19 +1,4 @@
-local indent = vim.api.nvim_create_augroup("indent", {clear = true})
-vim.api.nvim_create_autocmd("Filetype", {
-  pattern = "lua",
-  group = indent,
-  command = "setlocal ts=2 sts=2 sw=2"
-})
-vim.api.nvim_create_autocmd('Filetype', {
-  pattern = "ruby",
-  group = indent,
-  command = "setlocal ts=2 sts=2 sw=2"
-})
-vim.api.nvim_create_autocmd("Filetype", {
-  pattern = "yaml",
-  group = indent,
-  command = "setlocal ts=2 sts=2 sw=2"
-})
+-- local indent = vim.api.nvim_create_augroup("indent", {clear = true})
 
 local ft = vim.api.nvim_create_augroup("ft", {clear = true})
 vim.api.nvim_create_autocmd({"BufNewFile", "BufRead"}, {
--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -19,11 +19,11 @@ vim.opt.clipboard = "unnamedplus"
 vim.opt.mouse = "a"
 
 vim.opt.expandtab = true
-vim.opt.softtabstop = 4
-vim.opt.tabstop = 4
 vim.opt.scrolloff = 5
-vim.opt.shiftwidth = 4
+vim.opt.shiftwidth = 2
 vim.opt.sidescrolloff = 5
+vim.opt.softtabstop = 2
+vim.opt.tabstop = 2
 
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
