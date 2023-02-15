20de963f29a7a4901e180cbf25d1047a63e6e240
Cokile Ceoi <cokileceoi@outlook.com>
2023-02-15 17:51:25 +0800

    feat(nvim): use alt-n to switch to nth buffer

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -3,10 +3,24 @@ local mapping = require('utils.mapping')
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
+mapping.inoremap("jk", "<Esc>")
+
 mapping.nnoremap("<leader>s", ":w<CR>")
 mapping.nnoremap("<leader>l", ":noh<CR>")
 
 mapping.nnoremap("[b", ":bp<CR>")
 mapping.nnoremap("]b", ":bn<CR>")
 
-mapping.inoremap("jk", "<Esc>")
+-- Alt + number to switch to nth visible buffer
+for i = 1,9 do
+  mapping.nnoremap("<A-" .. i .. ">", function()
+    local lines = {}
+    for line in string.gmatch(vim.api.nvim_command_output("ls"), "[^\r?\n]+") do
+      table.insert(lines, line)
+    end
+    if #lines >= i then
+        local bufnr = string.match(lines[i], "^%s*(%d+)")
+        vim.api.nvim_set_current_buf(tonumber(bufnr))
+    end
+  end)
+end
